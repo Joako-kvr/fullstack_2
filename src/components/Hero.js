f1bd2d2599d7d12eec96082ee84d7c735890b613
PAAWSA09LC0914 <Duoc@PAAWSA09LC0914.duoc.acad>
2025-10-22 22:10:40 -0300

    segundo commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 
 function Hero() {
     return (
 
-    <div class="container">
-        <p class="mb-0">Hero</p>
-    </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-4" style={{ textAlign: "left" }}>
+                    <h1 className="mb-0">Energía para todos y todas</h1>
+                    <p>La energía del futuro al alcance de ti y tu familia. Revisa nuestro
+                        catálogo y proyecta tu cotización gratis.</p>
+                    <row>
+                        <Button variant="info">Info</Button>
+                        <Button variant="info">Info</Button>
+                    </row>
+                </div>
+                <div className="col-lg-4">
+                    <img src="ruta-de-tu-imagen.jpg" className="img-fluid" />
+                </div>
+            </div>
+        </div>
 
     )
 
